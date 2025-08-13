e8a39fb89e3a783a505d1e16ae87a82e0ca3a66a
Jerome-Seroma <darkenedhydra02@gmail.com>
2025-08-13 01:37:03 +0000

    ACT2

--- main.php
+++ main.php
@@ -3,14 +3,14 @@ $name = "Jerome";
 $age = "19";
 $school = "Global Reciprocal Colleges";
 $course = "Information Technology";
-$subject = "WEBSYS"
-$is$student = "Yes";
+$subject = "WEBSYS";
+$isStudent = "Yes";
 
-echo "My Name is " $name \n<br>" ;
-echo "My Age is " $age \n<br>";
-echo "I Study At " $school \n<br>";
-echo "My Course is " $course \n<br>";
-echo "My Subject is " $subject \n<br>";
-echo "Student? $isstudent<br>";
+echo "My Name is $name<br>" ;
+echo "My Age is $age<br>";
+echo "I Study At $school<br>";
+echo "My Course is $course<br>";
+echo "My Subject is $subject<br>";
+echo "Student? $isStudent<br>";
 
 ?>
