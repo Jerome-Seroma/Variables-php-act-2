76b9c1898b81993eb941787dc964dbe5ab43c68d
Jerome-Seroma <darkenedhydra02@gmail.com>
2025-08-13 01:36:00 +0000

    ACT2

--- main.php
+++ main.php
@@ -4,13 +4,13 @@ $age = "19";
 $school = "Global Reciprocal Colleges";
 $course = "Information Technology";
 $subject = "WEBSYS"
-$student = true;
+$is$student = "Yes";
 
 echo "My Name is " $name \n<br>" ;
 echo "My Age is " $age \n<br>";
 echo "I Study At " $school \n<br>";
 echo "My Course is " $course \n<br>";
 echo "My Subject is " $subject \n<br>";
-echo "Student: " .($student ? "Yes" : "No") . "\n<br>";
+echo "Student? $isstudent<br>";
 
 ?>
