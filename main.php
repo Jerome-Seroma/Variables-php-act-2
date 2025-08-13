c04f800f2cd1308203a3a83dfdc669e230c66453
Jerome-Seroma <darkenedhydra02@gmail.com>
2025-08-13 01:28:31 +0000

    ACT 2

---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Jerome";
+$age = "19";
+$school = "Global Reciprocal Colleges";
+$course = "Information Technology";
+$subject = "WEBSYS"
+$student = true;
+
+echo "My Name is " $name \n<br>" ;
+echo "My Age is " $age \n<br>";
+echo "I Study At " $school \n<br>";
+echo "My Course is " $course \n<br>";
+echo "My Subject is " $subject \n<br>";
+echo "Student: " .($student ? "Yes" : "No") . \n<br>";
+
+?>
